bc6f5fc14ca726b339c14c97853c90f9ca6b8f65
mkshing <mktshing4@gmail.com>
2023-05-19 20:11:27 +0900

    add results

--- harness.sh
+++ harness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account="stablegpt"
-#SBATCH --job-name="jp-eval-abeja2.7b-jsquad"
+#SBATCH --job-name="jp-eval"
 #SBATCH --partition=g40
 #SBATCH --cpus-per-task=12
 #SBATCH --gpus=1
@@ -8,20 +8,23 @@
 #SBATCH --output=/fsx/home-mkshing/slurm_outs/%x_%j.out
 #SBATCH --error=/fsx/home-mkshing/slurm_outs/%x_%j.err
 
-source /fsx/home-mkshing/venv/nlp/bin/activate
-# MODEL_ARGS="pretrained=abeja/gpt-neox-japanese-2.7b,low_cpu_mem_usage=True"
-MODEL_ARGS="pretrained=rinna/japanese-gpt-1b,use_fast=False"
-# MODEL_ARGS="pretrained=naclbit/gpt-j-japanese-6.8b,low_cpu_mem_usage=True" <- hasn't released the weight yet
-# MODEL_ARGS="pretrained=/fsx/jp-llm/hf_model/test,tokenizer=/fsx/home-mkshing/models/novelai-tokenizer,use_fast=False"
-TASK="jsquad" # jsquad, jaquad, jcommonsenseqa, lambada_openai_mt_ja
- # num_fewshot:: jsquad/jaquad -> 2, jcommonsenseqa -> 3, lambada_openai_mt_ja -> 0
+micromamba activate stable-neox-env
+# MODEL_ARGS="pretrained=abeja/gpt-neox-japanese-2.7b"
+# MODEL_ARGS="pretrained=rinna/japanese-gpt-1b,use_fast=False"
+# MODEL_ARGS="pretrained=rinna/japanese-gpt-neox-3.6b,use_fast=False"
+# MODEL_ARGS="pretrained=cyberagent/open-calm-1b"
+# MODEL_ARGS="pretrained=cyberagent/open-calm-3b"
+# MODEL_ARGS="pretrained=cyberagent/open-calm-7b"
+# MODEL_ARGS="pretrained=rinna/japanese-gpt2-xsmall,use_fast=False"
+MODEL_ARGS="pretrained=/fsx/jp-llm/hf_model/1b-ja-230b,tokenizer=/fsx/home-mkshing/models/novelai-tokenizer,use_fast=False"
+TASK="jsquad-1.1-0.2,jcommonsenseqa-1.1-0.2,jnli-1.1-0.2,marc_ja-1.1-0.2"
 python main.py \
     --model hf-causal \
     --model_args $MODEL_ARGS \
     --tasks $TASK \
-    --num_fewshot 2 \
+    --num_fewshot "2,3,3,3" \
     --device "cuda" \
-    --no_cache
+    --output_path "lm_outputs/stablelm-1b-ja-230b.json"
 
 # [Example] From gpt-neox's checkpoints
 # python ./deepy.py evaluate.py \
@@ -29,9 +32,4 @@ python main.py \
 #     --eval_tasks lambada_openai_mt_ja \
 #     --eval_num_fewshot 2
 
-# # srun --account="stablegpt" --partition=g40 --gpus=1 --cpus-per-gpu=12 --mem-per-cpu=11G --job-name="jp_eval" --pty bash -i
-# python main.py \
-#     --model hf-causal \
-#     --model_args "pretrained=/fsx/jp-llm/hf_model/test,tokenizer=/fsx/home-mkshing/models/novelai-tokenizer,use_fast=False" \
-#     --tasks lambada_openai_mt_ja \
-#     --device "cuda"
+# srun --account="stablegpt" --partition=g40 --gpus=1 --cpus-per-gpu=12 --mem-per-cpu=11G --job-name="jp_eval" --pty bash -i
