56864292a96b203b72fcd2defee5b84a8a7d99ce
mkshing <mktshing4@gmail.com>
2023-06-22 12:34:27 +0900

    add results of xlsum_ja

--- models/rinna/rinna-japanese-gpt-neox-3.6b-instruction-sft-v2/harness.sh
+++ models/rinna/rinna-japanese-gpt-neox-3.6b-instruction-sft-v2/harness.sh
@@ -1,3 +1,3 @@
 MODEL_ARGS="pretrained=rinna/japanese-gpt-neox-3.6b-instruction-sft-v2,use_fast=False"
-TASK="jsquad-1.1-0.4,jcommonsenseqa-1.1-0.4,jnli-1.1-0.4,marc_ja-1.1-0.4"
-python main.py     --model hf-causal     --model_args $MODEL_ARGS     --tasks $TASK     --num_fewshot "2,3,3,3"     --device "cuda"     --output_path "models/rinna-japanese-gpt-neox-3.6b-instruction-sft-v2/result.json"
+TASK="jcommonsenseqa-1.1-0.4,jnli-1.1-0.4,marc_ja-1.1-0.4,jsquad-1.1-0.4,xlsum_ja-1.0-0.4"
+python main.py --model hf-causal --model_args $MODEL_ARGS --tasks $TASK --num_fewshot "2,3,3,3,1" --device "cuda" --output_path "models/rinna-japanese-gpt-neox-3.6b-instruction-sft-v2/result.json"
