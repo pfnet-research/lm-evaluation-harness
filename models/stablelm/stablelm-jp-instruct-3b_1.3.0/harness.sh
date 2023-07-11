f81b57e869f546dff8147c0eea5d24d414fc79bb
fujiki <fujiki.nakamura@gmail.com>
2023-07-11 22:54:06 +0900

    small refactor

--- models/stablelm/stablelm-jp-instruct-3b_1.3.0/harness.sh
+++ models/stablelm/stablelm-jp-instruct-3b_1.3.0/harness.sh
@@ -2,7 +2,9 @@
 set -eu
 
 PROJECT_DIR=""
-MODEL_ARGS="pretrained=${PROJECT_DIR}/stablelm-jp-instruct-3b_1.3.0/,tokenizer=${PROJECT_DIR}/tokenizers/nai-hf-tokenizer/,use_fast=False"
+PRETRAINED_ARGS="pretrained=${PROJECT_DIR}/instruction_tuning/outputs/stablelm-jp-instruct-3b_1.3.0/"
+TOKENIZER_ARGS="tokenizer=${PROJECT_DIR}/tokenizers/nai-hf-tokenizer/,use_fast=False"
+MODEL_ARGS="${PRETRAINED_ARGS},${TOKENIZER_ARGS}"
 TASK="jsquad-1.1-0.3,jcommonsenseqa-1.1-0.3,jnli-1.1-0.3,marc_ja-1.1-0.3"
 NUM_FEW_SHOTS="2,3,3,3"
 python main.py \
