b0334ba092f5dff292f0229a90b915b886eec602
fujiki <fujiki.nakamura@gmail.com>
2023-07-13 19:03:53 +0900

    update result for stablelm-jp-instruct-3b_1.5.0

--- models/stablelm/stablelm-jp-instruct-3b_1.5.0/harness.sh
+++ models/stablelm/stablelm-jp-instruct-3b_1.5.0/harness.sh
@@ -4,9 +4,9 @@ set -eu
 
 PROJECT_DIR=""
 MODEL_NAME="stablelm-jp-instruct-3b_1.5.0"
-PRETRAINED_ARGS="pretrained=${PROJECT_DIR}/sft/checkpoints/${MODEL_NAME}"
-TOKENIZER_ARGS="tokenizer=${PROJECT_DIR}/tokenizers/nai-hf-tokenizer/,use_fast=False"
-MODEL_ARGS="${PRETRAINED},${TOKENIZER}"
+PRETRAINED="${PROJECT_DIR}/sft/checkpoints/${MODEL_NAME}"
+TOKENIZER="${PROJECT_DIR}/tokenizers/nai-hf-tokenizer/,use_fast=False"
+MODEL_ARGS="pretrained=${PRETRAINED},tokenizer=${TOKENIZER}"
 TASK="jsquad-1.1-0.3,jcommonsenseqa-1.1-0.3,jnli-1.1-0.3,marc_ja-1.1-0.3"
 OUTPUT_PATH="models/stablelm/${MODEL_NAME}/result.json"
 python main.py \
