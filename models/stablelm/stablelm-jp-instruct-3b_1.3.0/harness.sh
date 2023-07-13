18be8e72a06e9e1c4dd95504edba3df0157ca82c
fujiki <fujiki.nakamura@gmail.com>
2023-07-13 19:02:20 +0900

    add result for stablelm-jp-instruct-3b_1.3.0

--- models/stablelm/stablelm-jp-instruct-3b_1.3.0/harness.sh
+++ models/stablelm/stablelm-jp-instruct-3b_1.3.0/harness.sh
@@ -2,15 +2,17 @@
 set -eu
 
 PROJECT_DIR=""
-PRETRAINED_ARGS="pretrained=${PROJECT_DIR}/instruction_tuning/outputs/stablelm-jp-instruct-3b_1.3.0/"
-TOKENIZER_ARGS="tokenizer=${PROJECT_DIR}/tokenizers/nai-hf-tokenizer/,use_fast=False"
-MODEL_ARGS="${PRETRAINED_ARGS},${TOKENIZER_ARGS}"
+PRETRAINED="${PROJECT_DIR}/sft/checkpoints/stablelm-jp-instruct-3b_1.3.0/"
+TOKENIZER="${PROJECT_DIR}/tokenizers/nai-hf-tokenizer/,use_fast=False"
+MODEL_ARGS="pretrained=${PRETRAINED},tokenizer=${TOKENIZER}"
 TASK="jsquad-1.1-0.3,jcommonsenseqa-1.1-0.3,jnli-1.1-0.3,marc_ja-1.1-0.3"
 NUM_FEW_SHOTS="2,3,3,3"
+OUTPUT_PATH="models/stablelm-jp-instruct-3b_1.3.0/result.json"
 python main.py \
     --model hf-causal \
     --model_args $MODEL_ARGS \
     --tasks $TASK \
     --num_fewshot $NUM_FEW_SHOTS \
     --device "cuda" \
-    --output_path "models/stablelm-jp-instruct-3b_1.3.0/result.json"
+    --no_cache \
+    --output_path $OUTPUT_PATH
