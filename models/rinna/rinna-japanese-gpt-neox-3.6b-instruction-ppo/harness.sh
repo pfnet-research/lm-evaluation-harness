fc0e0fa6e68e29b3f4c5ff96aee41b972c2b08c2
kumapo <kumapo@users.noreply.github.com>
2023-07-16 16:27:47 +0900

    evaluate rinna/japanese-gpt-neox-3.6b-instruction-ppo

--- models/rinna/rinna-japanese-gpt-neox-3.6b-instruction-ppo/harness.sh
+++ models/rinna/rinna-japanese-gpt-neox-3.6b-instruction-ppo/harness.sh
@@ -1,3 +1,3 @@
-MODEL_ARGS="pretrained=rinna/japanese-gpt-neox-3.6b-instruction-ppo,use_fast=False"
+MODEL_ARGS="pretrained=rinna/japanese-gpt-neox-3.6b-instruction-ppo,use_fast=False,device_map=auto,torch_dtype=auto"
 TASK="jcommonsenseqa-1.1-0.4,jnli-1.1-0.4,marc_ja-1.1-0.4,jsquad-1.1-0.4,xlsum_ja-1.0-0.4"
 python main.py --model hf-causal --model_args $MODEL_ARGS --tasks $TASK --num_fewshot "2,3,3,3,1" --device "cuda" --output_path "models/rinna-japanese-gpt-neox-3.6b-instruction-ppo/result.json"
